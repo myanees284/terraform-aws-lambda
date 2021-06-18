f4494772fd1e24da8a411c1f01a0d6dff2d87718
myanees284 <anees2184@gmail.com>
2021-06-19 00:08:31 +0400

    code change to include py files in seperate folder

--- local.tf
+++ local.tf
@@ -1,6 +1,6 @@
 locals {
-  lambda_zip_location  = "outputs/${var.lambdaCodeFile}.zip"
-  lambda_function_name = replace("${var.lambdaCodeFile}", "/\\..*/", "")
+  lambda_zip_location  = "outputs/${replace("${var.lambdaCodeFile}", "/.*/([^.]+).*/", "$1")}.zip"
+  lambda_function_name = replace("${var.lambdaCodeFile}", "/.*/([^.]+).*/", "$1")
   lambda_invoke_urn    = aws_lambda_function.lambda.invoke_arn
   lambda_name          = aws_lambda_function.lambda.function_name
 }
